5b5aa13b757402e09f818434f873dccd6085b174
nkbel <nguyend@wlhs.wlwv.k12.or.us>
2023-02-06 23:17:08 -0800

    e

--- Polygon.java
+++ Polygon.java
@@ -1,20 +1,50 @@
 import java.text.DecimalFormat;
+
+/**
+ * @file Polygon.java
+ * @date 2/6/2023
+ * @author Dylan Nguyen
+ *
+ * @description Creates a polygon with a side number, side length, and shape type, which the user can access and
+ * print.
+ */
+ */
 public class Polygon {
+     //instance variables
     private int numSides;
     private double sideLength;
     private String shapeType;
 
-    //constructors
+    //default constructor
+
+    /**
+     * @description Default constructor that creates a basic triangle with a side length of 1.0
+     */
     public Polygon(){
-        numSides = 0;
-        sideLength = 0.0;
-        shapeType = "";
+        numSides = 3;
+        sideLength = 1.0;
+        shapeType = "triangle";
     }
 
+    //overloaded constructor
+
+    /**
+     * @description Creates a Polygon object with specified side number, side length, and shape type. If the
+     *      * parameters are not greater than 0, they are set to 1.0.
+     * @param s number of sides in the Polygon
+     * @param sl length of each side in the Polygon
+     * @param st type of Polygon shape
+     */
     public Polygon (int s, double sl, String st){
-        s = numSides;
-        sl = sideLength;
+
+        numSides = 1;
+        sideLength = 1.0;
         st = shapeType;
+        if(s > 0){
+            numSides = s;
+        }
+        if(sl > 0){
+            sideLength = sl;
 
     }
 
